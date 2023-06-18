7becc65934e4ef1df3bc26768a810360b7162564
q-i-n-g <121088532+q-i-n-g@users.noreply.github.com>
2023-06-18 15:21:48 +0800

    Add files via upload

--- aircraft/serializers.py
+++ aircraft/serializers.py
@@ -2,25 +2,56 @@ from rest_framework import serializers
 from .models import *
 
 # 用户
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class User_infoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = user_info
-        fields = ['id_card', 'name', 'account', 'pwd', 'sex', 'telephone', 'type']
+        model = user_indo
+        fields = ['user_name',  'password', 'phone', 'email', 'user_type', 'point']
+
+#管理员
+class AdministratorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = administrator
+        fields = ['administrator_id',  'password', 'phone', 'email']
+
+
+#乘机人
+class Passenger_infoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = passenger_info
+        fields = ['passenger_identity_id',  'passenger_name', 'phone', 'sex','birthdate','passport','passenger_type']
+
+# 飞机
+class Airplane_infoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = airplane_info
+        fields = ['airplane_id', 'plane_type', 'company_id', 'economy_set', 'first_set', 'bussiness_set']
+
+#机场
+class Airport_infoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = airport_info
+        fields = ['airport_id', 'airport_name', 'location', 'time_zones']
 
 # 航班
-class FlightSerializer(serializers.HyperlinkedModelSerializer):
+class Flight_infoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = flight_info
-        fields = ['flight_num', 'corp', 'depart', 'arrive', 'depart_time', 'arrive_time', 'depart_airport', 'arrive_airport', 'ticket_total_first_class', 'ticket_total_economy_class', 'ticket_remain', 'price_first_class', 'price_economy_class', 'port']
+        fields = ['flight_num', 'depart_time', 'arrive_time', 'airplane_num', 'depart_airport', 'arrive_airport', 'current_first_set', 'current_economy_set', 'current_bussiness_set', 'ecnomy_class_price', 'first_class_price', 'bussiness_class_price', 'baggage_info']
 
-# 订单
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
+# 收藏
+class FavoritesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = order_info
-        fields = ['id', 'user_id', 'flight_id', 'time', 'ticket_id', 'cost', 'status']
+        model = favorites
+        fields = ['favorite_id', 'user_name', 'flight_num1', 'flight_num2', 'set_choice', 'set_class1', 'set_class2','total_price','transfer']
 
 # 票
-#class TicketSerializer(serializers.HyperlinkedModelSerializer):
-#    class Meta:
-#        model = ticket
-#        fields = ['id', 'corp', 'flight_id', 'user_id', 'ticket_type', 'boarding_time', 'seat_num', 'port', 'message']
+class Order_infoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = order_info
+        fields = ['order_id', 'user_name', 'passenger_identity_id', 'flight_num1', 'flight_num2', 'order_time',  'set_class1', 'set_class2','set_num1','set_num2','order_status','price','point_use']
+
+#用户乘机人绑定
+class Passenger_userSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = passenger_user
+        fields = ['user_name', 'passenger_identity_id']
